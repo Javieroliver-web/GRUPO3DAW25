35fc6221497d8748f0d6716a785e301ed7685ee2
Jose Carlos <josecarlos.sanchoac@gmail.com>
2025-11-14 12:13:59 +0100

    Integrante 3: Añadidos index.php, Controller.php y Config.php con datos de BD configurados

--- app/Config.php
+++ app/Config.php
@@ -2,10 +2,10 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos2025";
-     static public $mvc_bd_usuario  = "danielu";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_bd_hostname = "sql102.infinityfree.com";
+     static public $mvc_bd_nombre   = "if0_40357147_alimentos32025";
+     static public $mvc_bd_usuario  = "if0_40357147";
+     static public $mvc_bd_clave    = "KrW7FSdaahei";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
